6bc31bbaeddef94527a33138e5b5dfb4f63124e4
L IM <iml1111>
2019-07-11 16:49:55 +0900

    refac

--- test.py
+++ test.py
@@ -3,21 +3,23 @@ import requests
 import subprocess
 import hashlib
 
-size = 300
-# Write here
-input_text = "iml1111"
-file_name = input_text
-
 def md5_hex(text):
     m = hashlib.md5()
     m.update(text.encode('ascii', errors='ignore'))
     return m.hexdigest()
 
-output_file = os.path.join(file_name + '.png')
-if not os.path.exists(output_file):
-    grav_url = "http://www.gravatar.com/avatar/" + md5_hex(input_text) + "?d=identicon&s=" + str(size)
-    r = requests.get(grav_url)
-    if r.ok:
-        with open(output_file, 'wb') as img:
-            img.write(r.content)
-            print(grav_url)
+def convert(input_text = "test", size = 300):
+	output_file = os.path.join(input_text + '.png')
+	if not os.path.exists(output_file):
+	    grav_url = "http://www.gravatar.com/avatar/" + \
+	    md5_hex(input_text) + "?d=identicon&s=" + str(size)
+	    r = requests.get(grav_url)
+	    if r.ok:
+	        with open(output_file, 'wb') as img:
+	            img.write(r.content)
+	            print("complete! Here Image URL.")
+	            print(grav_url)
+
+if __name__ == '__main__':
+	convert(input("Input Your Text: "),
+		int(input("Input Size(Only int): ")))
